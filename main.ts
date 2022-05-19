589023b000f1e40da69ce65758d338c7d482dfb1
spirit160708 <103181350+spirit160708@users.noreply.github.com>
2022-05-19 11:21:10 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,27 @@
 basic.forever(function () {
-    if (input.acceleration(Dimension.Y) < -100) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.Y) > 100) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) < -100) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) > 100) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+    if (input.acceleration(Dimension.Y) < -200) {
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showArrow(ArrowNames.NorthWest)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showArrow(ArrowNames.NorthEast)
+        } else {
+            basic.showArrow(ArrowNames.North)
+        }
+    } else if (input.acceleration(Dimension.Y) > 200) {
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showArrow(ArrowNames.SouthWest)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showArrow(ArrowNames.SouthEast)
+        } else {
+            basic.showArrow(ArrowNames.South)
+        }
     } else {
-        basic.showIcon(IconNames.Yes)
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showArrow(ArrowNames.West)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showArrow(ArrowNames.East)
+        } else {
+            basic.showIcon(IconNames.Yes)
+        }
     }
 })
